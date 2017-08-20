5b876dae71fa5ba2707b185d6fb218292f5f0025
Stijn van der Wielen <stijnvanderwielen@gmail.com>
2017-08-20 22:51:52 +0200

    Writing more views.

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,15 @@
+"""This module handles all urls for the application"""
 from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    # ex: /polls/
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.result, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,22 @@
-#from django.shortcuts import render
+"""This module manages all views."""
 from django.http import HttpResponse
 
-# Create your views here.
+
 def index(request):
+    """Question “index” page – displays the latest few questions"""
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    """Question “detail” page – displays a question text,
+    with no results but with a form to vote.
+    """
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def result(request, question_id):
+    """Question “results” page – displays results for a particular question."""
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    """Vote action – handles voting for a particular choice in a particular question."""
+    return HttpResponse("You're voting on question %s." % question_id)
